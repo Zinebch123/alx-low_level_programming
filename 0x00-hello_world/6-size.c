1438a3ae5a5c955497b164b4fc3957bc2156080a
Zinebch123 <zinebchbil8@gmail.com>
2023-04-20 17:38:01 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - 6-size.c
+ * description - Write a C program that prints,
+ * the size of int,char,float,long int and long long int
+ * Return: 0
  */
 int main(void)
 {
-print("Size of a char : % d byte(s)\", sizeof(char));
-printf("Size of an int : % d byte(s)\", sizeof(int));
-printf("Size of a long int : % d byte(s)\", sizeof(long int));
-printf("Size of a long long int : % d byte(s)\", sizeof(long long int));
-printf("Size of a float : % d byte(s)\", sizeof(float));
-	return (0);
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
 }
