b22f54f01de7c4aa87d33b8832aa1feac1706517
Zinebch123 <zinebchbil8@gmail.com>
2023-05-02 13:52:49 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,14 +1,13 @@
+#include "main.h"
 #include <unistd.h>
-
 /**
- * _putchar - write the character to stdout
- * the character to print is ch
- * Return: 1 (success)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _putchar(char ch)
+int _putchar(char c)
 {
-
-return (write(1, &ch, 1));
+	return (write(1, &c, 1));
 }
-
