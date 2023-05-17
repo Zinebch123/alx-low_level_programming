1eb0da574c4016180297b8c964033f3dc8f7fa02
Zinebch123 <zinebchbil8@gmail.com>
2023-05-17 15:46:20 -0700

    h

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * _strlen - returns the length of a string.
+ * owned by sfiya
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
  */
+
 int _strlen(char *s)
 {
-        	int longi = 0;
- 
-        	while (*s != '\0')
-        	{
-                    	longi++;
-                    	s++;
-        	}
- 
-        	return (longi);
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
 }
